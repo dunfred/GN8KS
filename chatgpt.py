6cd24a1857b0c89d6c635db84a3583e7e899e71b
dunfred <donklenam2@gmail.com>
2024-07-22 17:07:45 +0000

    tweak made for GPT selector

--- chatgpt.py
+++ chatgpt.py
@@ -256,7 +256,7 @@ for task in JOBS['tasks']:
                 observed_element_locator, 
                 "Analyzed", 
                 idx+1,
-                turn_menu_item_locator = (By.XPATH, './/div[contains(@class, "mt-1") and contains(@class, "flex") and contains(@class, "gap-3") and contains(@class, "empty:hidden") and contains(@class, "ml-3")]')
+                turn_menu_item_locator = (By.XPATH, './/div[contains(@class, "mt-1") and contains(@class, "flex") and contains(@class, "gap-3") and contains(@class, "empty:hidden") and contains(@class, "ml-")]')
                 ))
 
             # Record the time when "Analysis complete" appears
@@ -272,7 +272,7 @@ for task in JOBS['tasks']:
 
             # Ensure the bot is truly done with analysis by looking for the 
             # action menu that every completed conversation has at the bottom
-            turn_menu_item_locator = (By.XPATH, './/div[contains(@class, "mt-1") and contains(@class, "flex") and contains(@class, "gap-3") and contains(@class, "empty:hidden") and contains(@class, "ml-3")]')
+            turn_menu_item_locator = (By.XPATH, './/div[contains(@class, "mt-1") and contains(@class, "flex") and contains(@class, "gap-3") and contains(@class, "empty:hidden") and contains(@class, "ml-")]')
             WebDriverWait(gpt_reponse_elem, 180).until(EC.presence_of_element_located(turn_menu_item_locator))
 
             # Grab all GPT code and text response blocks while maintaining order
